afff1241bb9e95f3b02bb9589a33b351970b661b
Abhimanue <abhimanue80@gmail.com>
2024-10-23 16:45:47 +0530

    fix errors

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+require('express-async-errors');
 require('dotenv').config()
+
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 const connectDB = require('./DB/connect')
 
@@ -7,10 +11,15 @@ const connectDB = require('./DB/connect')
 const authRouter = require('./routes/auth')
 
 const app =  express()
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.use('api/v1/auth',authRouter)
+// app.get('/',(req,res)=>{
+//     res.send('slack api')
+// })
+app.use('/api/v1/auth',authRouter)
 
+app.use(errorHandlerMiddleware);
 const PORT = process.env.PORT || 3000 ;
 const start = async()=>{
     try{
--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
+const bcrypt  = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const userSchema = new mongoose.Schema({
     name : {
         type : String,
-        required : [true, 'Please provide name']
+        required : [true, 'Please provide name'],
     },
     email: {
         type : String,
@@ -11,7 +14,7 @@ const userSchema = new mongoose.Schema({
         match : [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
             'please provide a valid email'
         ],
-        unique: [true,'Email id already registered'],
+        unique: true,
     },
     password : {
         type : String,
